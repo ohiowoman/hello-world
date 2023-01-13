02481051e28849ea4ba31aa6663a773bffe6901f
ohiowoman <122560318+ohiowoman@users.noreply.github.com>
2023-01-13 13:13:05 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Sword)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Skull)
+})
+basic.showIcon(IconNames.Yes)
